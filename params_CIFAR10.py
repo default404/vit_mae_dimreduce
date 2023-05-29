166da64a5fc93c0aae242d056bf4e3fce7b3ec84
klau <dennis.klau@iao.fraunhofer.de>
2023-05-29 16:01:26 +0200

    Initial commit.

---
+++ params_CIFAR10.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+# DATA
+BUFFER_SIZE = 1024
+BATCH_SIZE = 256
+AUTO = tf.data.AUTOTUNE
+INPUT_SHAPE = (32, 32, 3)
+IMAGE_CHANNELS = INPUT_SHAPE[-1]
+NUM_CLASSES = 10
+
+# PATHS
+MODEL_SAVE_PATH = "saved_models/ViT_CIFAR10/"
+
+# OPTIMIZER
+LEARNING_RATE = 5e-3
+WEIGHT_DECAY = 1e-4
+
+# PRETRAINING
+EPOCHS = 100
+
+# AUGMENTATION
+IMAGE_SIZE = 48  # We will resize input images to this size.
+PATCH_SIZE = 6  # Size of the patches to be extracted from the input images.
+NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
+MASK_PROPORTION = 0.75  # We have found 75% masking to give us the best results.
+
+# ENCODER and DECODER
+LAYER_NORM_EPS = 1e-6
+ENC_PROJECTION_DIM = 128
+DEC_PROJECTION_DIM = 64
+ENC_NUM_HEADS = 4
+ENC_LAYERS = 6
+DEC_NUM_HEADS = 4
+DEC_LAYERS = (
+    2  # The decoder is lightweight but should be reasonably deep for reconstruction.
+)
+ENC_TRANSFORMER_UNITS = [
+    ENC_PROJECTION_DIM * 2,
+    ENC_PROJECTION_DIM,
+]  # Size of the transformer layers.
+DEC_TRANSFORMER_UNITS = [
+    DEC_PROJECTION_DIM * 2,
+    DEC_PROJECTION_DIM,
+]
